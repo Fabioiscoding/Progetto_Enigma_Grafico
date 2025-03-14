f08ad473c4e312662cd7c1c3bd216831abcd9be3
Fabioiscoding <gurjinder07italy@gmail.com>
2025-03-14 21:59:58 +0100

    commit

--- src/main/java/singh/g/enigmagraphics/EnigmaApplication.java
+++ src/main/java/singh/g/enigmagraphics/EnigmaApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class EnigmaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(EnigmaApplication.class.getResource("enigma-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Enigma");
         stage.setScene(scene);
         stage.show();
     }
